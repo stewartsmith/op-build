5c065a55e49a549adae95cbfdebcf6e59d93cff8
Stewart Smith <stewart@linux.ibm.com>
2018-12-04 22:35:50 -0600

    Use pure ffspart to construct POWER8 PNOR
    We make the traditional method optional (although it's not option if
    building a POWER9 PNOR due to the signing work that's done).
    
    We discover a few more quirks we have to deal with, including moving a
    lot of weird Hostboot image assembly over to the hostboot-p8.mk
    makefile.
    
    Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

--- openpower/package/hostboot-p8/hostboot-p8.mk
+++ openpower/package/hostboot-p8/hostboot-p8.mk
@@ -27,6 +27,33 @@ endef
 
 define HOSTBOOT_P8_INSTALL_IMAGES_CMDS
         cd $(@D) && source ./env.bash && $(@D)/src/build/tools/hbDistribute --openpower $(STAGING_DIR)/hostboot_build_images/
+
+	# These steps should really be in the Hostboot Makefiles.
+	# They were formerly in update_image.pl in the pnor repo
+
+	# Add SBE/normal headers and inject ECC into HBB (hostboot base) partition binary
+	echo "00000000001800000000000008000000000000000007EF80" | xxd -r -ps - $(STAGING_DIR)/hostboot_build_images/sbe.header
+	env echo -en VERSION\\0 > $(STAGING_DIR)/hostboot_build_images/hostboot.sha.bin
+	sha512sum $(STAGING_DIR)/hostboot_build_images/img/hostboot.bin | awk '{print $1}' | xxd -pr -r >> $(STAGING_DIR)/hostboot_build_images/hostboot.sha.bin
+	dd if=$(STAGING_DIR)/hostboot_build_images/hostboot.sha.bin of=$(STAGING_DIR)/hostboot_build_images/secureboot.header ibs=4k conv=sync
+	cat $(STAGING_DIR)/hostboot_build_images/sbe.header $(STAGING_DIR)/hostboot_build_images/secureboot.header $(STAGING_DIR)/hostboot_build_images/img/hostboot.bin > $(STAGING_DIR)/hostboot_build_images/hostboot.stage.bin
+	dd if=$(STAGING_DIR)/hostboot_build_images/hostboot.stage.bin of=$(STAGING_DIR)/hostboot_build_images/hostboot.header.bin ibs=512k conv=sync
+
+	# Add header and inject ECC into HBI (hostboot extended) partition binary
+	env echo -en VERSION\\0 > $(STAGING_DIR)/hostboot_build_images/hostboot_extended.sha.bin
+	sha512sum $(STAGING_DIR)/hostboot_build_images/img/hostboot_extended.bin | awk '{print $1}' | xxd -pr -r >> $(STAGING_DIR)/hostboot_build_images/hostboot_extended.sha.bin
+	dd if=$(STAGING_DIR)/hostboot_build_images/hostboot_extended.sha.bin of=$(STAGING_DIR)/hostboot_build_images/hostboot.temp.bin ibs=4k conv=sync
+	cat $(STAGING_DIR)/hostboot_build_images/img/hostboot_extended.bin >> $(STAGING_DIR)/hostboot_build_images/hostboot.temp.bin
+	dd if=$(STAGING_DIR)/hostboot_build_images/hostboot.temp.bin of=$(STAGING_DIR)/hostboot_build_images/hostboot_extended.header.bin ibs=5120k conv=sync
+
+	# Add header and inject ECC into HBRT (hostboot runtime) partition binary
+	env echo -en VERSION\\0 > $(STAGING_DIR)/hostboot_build_images/hostboot_runtime.sha.bin
+	sha512sum $(STAGING_DIR)/hostboot_build_images/img/hostboot_runtime.bin | awk '{print $1}' | xxd -pr -r >> $(STAGING_DIR)/hostboot_build_images/hostboot_runtime.sha.bin
+	dd if=$(STAGING_DIR)/hostboot_build_images/hostboot_runtime.sha.bin of=$(STAGING_DIR)/hostboot_build_images/hostboot.temp.bin ibs=4k conv=sync
+	cat $(STAGING_DIR)/hostboot_build_images/img/hostboot_runtime.bin >> $(STAGING_DIR)/hostboot_build_images/hostboot.temp.bin
+	dd if=$(STAGING_DIR)/hostboot_build_images/hostboot.temp.bin of=$(STAGING_DIR)/hostboot_build_images/hostboot_runtime.header.bin ibs=3072K conv=sync
+
+
 endef
 
 $(eval $(generic-package))
--- openpower/package/openpower-pnor/openpower-pnor.mk
+++ openpower/package/openpower-pnor/openpower-pnor.mk
@@ -105,121 +105,38 @@ endif
 
 define OPENPOWER_PNOR_INSTALL_IMAGES_CMDS
         mkdir -p $(OPENPOWER_PNOR_SCRATCH_DIR)
-
-        $(TARGET_MAKE_ENV) $(@D)/update_image.pl \
-            -release  $(OPENPOWER_RELEASE) \
-            -op_target_dir $(HOSTBOOT_IMAGE_DIR) \
-            -hb_image_dir $(HOSTBOOT_IMAGE_DIR) \
-            -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
-            -hb_binary_dir $(HOSTBOOT_BINARY_DIR) \
-            -hcode_dir $(HCODE_STAGING_DIR) \
-            -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME) \
-            -targeting_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME) \
-            -targeting_RO_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME).protected \
-            -targeting_RO_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME).protected \
-            -targeting_RW_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME).unprotected \
-            -targeting_RW_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME).unprotected \
-            -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) \
-            -sbe_binary_dir $(SBE_BINARY_DIR) \
-            -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) \
-            -wink_binary_filename $(BR2_HOSTBOOT_BINARY_WINK_FILENAME) \
-            -occ_binary_filename $(OCC_STAGING_DIR)/$(OCC_BIN_FILENAME) \
-            -capp_binary_filename $(BINARIES_DIR)/$(BR2_CAPP_UCODE_BIN_FILENAME) \
-            -ima_catalog_binary_filename $(BINARIES_DIR)/$(BR2_IMA_CATALOG_FILENAME) \
-            -openpower_version_filename $(OPENPOWER_PNOR_VERSION_FILE) \
-            -wof_binary_filename $(OPENPOWER_MRW_SCRATCH_DIR)/$(BR2_WOFDATA_FILENAME) \
-            -memd_binary_filename $(OPENPOWER_MRW_SCRATCH_DIR)/$(BR2_MEMDDATA_FILENAME) \
-            -payload $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_NAME) \
-            -payload_filename $(BR2_SKIBOOT_LID_XZ_NAME) \
-            -binary_dir $(BINARIES_DIR) \
-            -bootkernel_filename $(LINUX_IMAGE_NAME) \
-            -pnor_layout $(@D)/"$(OPENPOWER_RELEASE)"Layouts/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) \
-            $(XZ_ARG) $(KEY_TRANSITION_ARG) $(SIGN_MODE_ARG) \
-
         mkdir -p $(STAGING_DIR)/pnor/
-        $(TARGET_MAKE_ENV) $(@D)/create_pnor_image.pl \
-            -release $(OPENPOWER_RELEASE) \
-            -xml_layout_file $(@D)/"$(OPENPOWER_RELEASE)"Layouts/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) \
-            -pnor_filename $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) \
-            -hb_image_dir $(HOSTBOOT_IMAGE_DIR) \
-            -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
-            -outdir $(STAGING_DIR)/pnor/ \
-            -payload $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_SKIBOOT_LID_XZ_NAME) \
-            -bootkernel $(OPENPOWER_PNOR_SCRATCH_DIR)/$(LINUX_IMAGE_NAME) \
-            -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) \
-            -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) \
-            -wink_binary_filename $(BR2_HOSTBOOT_BINARY_WINK_FILENAME) \
-            -occ_binary_filename $(OCC_STAGING_DIR)/$(OCC_BIN_FILENAME) \
-            -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME) \
-            -targeting_RO_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME).protected \
-            -targeting_RW_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME).unprotected \
-            -wofdata_binary_filename $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_WOFDATA_BINARY_FILENAME) \
-            -memddata_binary_filename $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_MEMDDATA_BINARY_FILENAME) \
-            -openpower_version_filename $(OPENPOWER_PNOR_SCRATCH_DIR)/openpower_pnor_version.bin
-
-        $(INSTALL) $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(BINARIES_DIR)
-	if [ "$(BR2_PACKAGE_HOSTBOOT_P8)" == "y" ]; then \
-		$(TARGET_MAKE_ENV) ../openpower/scripts/pnordiff.sh $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(STAGING_DIR)/pnor/ffspart.pnor ; \
-	fi
 
 	# Now construct a PNOR exclusively using ffspart
+
 	# Step 1: copy all the partition files over.
 	if [ "$(BR2_PACKAGE_HOSTBOOT_P8)" == "y" ]; then \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot.header.bin $(STAGING_DIR)/pnor/ ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot_extended.header.bin $(STAGING_DIR)/pnor/ ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot_runtime.header.bin $(STAGING_DIR)/pnor/ ; \
-	fi
-	# We don't (yet) do the STB header
-	if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot.header.bin.ecc $(STAGING_DIR)/pnor/ ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot_extended.header.bin.ecc $(STAGING_DIR)/pnor/ ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot_runtime.header.bin.ecc $(STAGING_DIR)/pnor/ ; \
+		cp $(HOSTBOOT_IMAGE_DIR)/hostboot.header.bin $(STAGING_DIR)/pnor/ ; \
+		cp $(HOSTBOOT_IMAGE_DIR)/hostboot_extended.header.bin $(STAGING_DIR)/pnor/ ; \
+		cp $(HOSTBOOT_IMAGE_DIR)/hostboot_runtime.header.bin $(STAGING_DIR)/pnor/ ; \
+		cp $(HOSTBOOT_BINARY_DIR)/$(BR2_HOSTBOOT_BINARY_WINK_FILENAME) $(STAGING_DIR)/pnor/wink.hdr.bin.ecc ; \
+		cp $(HOSTBOOT_IMAGE_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME) $(STAGING_DIR)/pnor/hostboot_targeting.bin ; \
+		cp $(HOSTBOOT_BINARY_DIR)/$(BR2_HOSTBOOT_BINARY_SBE_FILENAME) $(STAGING_DIR)/pnor/sbe.img.ecc ; \
 	fi
 
-	# POWER9 only (or, rather, not p8)
-        if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hbbl.bin.ecc $(STAGING_DIR)/pnor/hbbl.bin.ecc; \
-		$(TARGET_MAKE_ENV) python $(SBE_BINARY_DIR)/sbeOpDistribute.py --install \
-			--buildSbePart $(HOSTBOOT_IMAGE_DIR)/buildSbePart.pl \
-			--hw_ref_image $(HCODE_STAGING_DIR)/p9n.ref_image.bin \
-			--sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) \
-			--scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
-			--sbe_binary_dir $(SBE_BINARY_DIR) ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/ringOvd.bin $(STAGING_DIR)/pnor/ ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/SBKT.bin $(STAGING_DIR)/pnor/SBKT.bin.ecc ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_WOFDATA_BINARY_FILENAME) $(STAGING_DIR)/pnor/wofdata.bin.ecc ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_MEMDDATA_BINARY_FILENAME) $(STAGING_DIR)/pnor/memd_extra_data.bin.ecc ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_HOSTBOOT_BINARY_WINK_FILENAME) $(STAGING_DIR)/pnor/hcode.bin.ecc ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME).ecc $(STAGING_DIR)/pnor/hostboot_targeting.bin.ecc ; \
+	if [ -f $(HOSTBOOT_IMAGE_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME).protected ]; then \
+		cp $(HOSTBOOT_IMAGE_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME).protected $(STAGING_DIR)/pnor/hostboot_targeting.bin.protected ; \
+		cp $(HOSTBOOT_IMAGE_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME).unprotected $(STAGING_DIR)/pnor/hostboot_targeting.bin.unprotected ; \
 	fi
-	if [ "$(BR2_PACKAGE_HOSTBOOT_P8)" == "y" ]; then \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_HOSTBOOT_BINARY_WINK_FILENAME) $(STAGING_DIR)/pnor/wink.hdr.bin.ecc ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME) $(STAGING_DIR)/pnor/hostboot_targeting.bin ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_HOSTBOOT_BINARY_SBE_FILENAME) $(STAGING_DIR)/pnor/sbe.img.ecc ; \
-	fi
-
-	if [ -f $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_OPENPOWER_TARGETING_ECC_FILENAME).protected ]; then \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME).protected $(STAGING_DIR)/pnor/hostboot_targeting.bin.protected ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME).unprotected $(STAGING_DIR)/pnor/hostboot_targeting.bin.unprotected ; \
-	fi
-	if [ -f $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) ]; then \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) $(STAGING_DIR)/pnor/sbec.img.ecc ; \
+	if [ -f $(HOSTBOOT_BINARY_DIR)/$(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) ]; then \
+		cp $(HOSTBOOT_BINARY_DIR)/$(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) $(STAGING_DIR)/pnor/sbec.img.ecc ; \
 	fi
 
 	# Some signed partitions we still have to get from update_image.pl
+	# We pad VERSION to 4k with zeros as per what update_image.pl does
+	# We also have to XZ compress skiboot ourselves with worse compression to try
+	# to get the same binary as update_image.pl
         if [ "$(BR2_PACKAGE_HOSTBOOT_P8)" == "y" ]; then \
 		cp $(BINARIES_DIR)/$(BR2_CAPP_UCODE_BIN_FILENAME) $(STAGING_DIR)/pnor/cappucode.bin ; \
 		cp $(BINARIES_DIR)/$(BR2_IMA_CATALOG_FILENAME) $(STAGING_DIR)/pnor/ima_catalog.bin ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_SKIBOOT_LID_XZ_NAME) $(STAGING_DIR)/pnor/skiboot.lid.xz ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(LINUX_IMAGE_NAME) $(STAGING_DIR)/pnor/zImage.epapr ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/openpower_pnor_version.bin $(STAGING_DIR)/pnor/openpower_pnor_version.bin ; \
-	fi
-        if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/cappucode.bin.ecc $(STAGING_DIR)/pnor/cappucode.bin.ecc ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/ima_catalog.bin.ecc $(STAGING_DIR)/pnor/ima_catalog.bin.ecc ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_SKIBOOT_LID_XZ_NAME) $(STAGING_DIR)/pnor/skiboot.lid.xz.stb ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(LINUX_IMAGE_NAME) $(STAGING_DIR)/pnor/zImage.epapr.stb ; \
-		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/openpower_pnor_version.bin $(STAGING_DIR)/pnor/openpower_pnor_version.bin ; \
+		xz -fk --stdout --check=crc32 $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_NAME) > $(STAGING_DIR)/pnor/skiboot.lid.xz ; \
+		cp $(BINARIES_DIR)/$(LINUX_IMAGE_NAME) $(STAGING_DIR)/pnor/zImage.epapr ; \
+		dd if=$(OPENPOWER_PNOR_VERSION_FILE) of=$(STAGING_DIR)/pnor/openpower_pnor_version.bin ibs=4K conv=sync ; \
 	fi
 
 
@@ -259,10 +176,6 @@ define OPENPOWER_PNOR_INSTALL_IMAGES_CMDS
 	if [ "$(BR2_PACKAGE_HOSTBOOT_P8)" == "y" ]; then \
 		dd if=$(OCC_STAGING_DIR)/$(OCC_BIN_FILENAME) of=$(STAGING_DIR)/pnor/occ.bin ibs=1M conv=sync ; \
 	fi
-	# On P9 we don't need to pad OCC, but we do have the secboot header
-	if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
-		cp $(OCC_STAGING_DIR)/$(OCC_BIN_FILENAME).ecc $(STAGING_DIR)/pnor/occ.bin.header.ecc ; \
-	fi
 
 	# NVRAM is zeros, notably *not* a valid NVRAM partition format
 	# but only on POWER8!?!
@@ -273,8 +186,117 @@ define OPENPOWER_PNOR_INSTALL_IMAGES_CMDS
 	# Step 2: build the pnor
 	rm -f $(STAGING_DIR)/pnor/ffspart2.pnor
 	touch  $(STAGING_DIR)/pnor/ffspart2.pnor
-	(cd $(STAGING_DIR)/pnor; $(TARGET_MAKE_ENV) ffspart -e -s $(BR2_OPENPOWER_PNOR_BLOCK_SIZE) -c $(BR2_OPENPOWER_PNOR_BLOCK_COUNT) -i $(abspath $(BR2_OPENPOWER_PNOR_CSV_LAYOUT_FILENAME)) -p $(STAGING_DIR)/pnor/ffspart2.pnor)
-	$(TARGET_MAKE_ENV) ../openpower/scripts/pnordiff.sh $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(STAGING_DIR)/pnor/ffspart2.pnor
+	# Generate it for P8 now, P9 has to come after update_image/create_image for now.
+	if [ "$(BR2_PACKAGE_HOSTBOOT_P8)" == "y" ]; then \
+		(cd $(STAGING_DIR)/pnor; $(TARGET_MAKE_ENV) ffspart -e -s $(BR2_OPENPOWER_PNOR_BLOCK_SIZE) -c $(BR2_OPENPOWER_PNOR_BLOCK_COUNT) -i $(abspath $(BR2_OPENPOWER_PNOR_CSV_LAYOUT_FILENAME)) -p $(STAGING_DIR)/pnor/ffspart2.pnor) ; \
+	fi
+
+	if [ "$(BR2_OPENPOWER_PNOR_OLD_METHOD)" == "y" ]; then \
+        $(TARGET_MAKE_ENV) $(@D)/update_image.pl \
+            -release  $(OPENPOWER_RELEASE) \
+            -op_target_dir $(HOSTBOOT_IMAGE_DIR) \
+            -hb_image_dir $(HOSTBOOT_IMAGE_DIR) \
+            -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
+            -hb_binary_dir $(HOSTBOOT_BINARY_DIR) \
+            -hcode_dir $(HCODE_STAGING_DIR) \
+            -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME) \
+            -targeting_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME) \
+            -targeting_RO_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME).protected \
+            -targeting_RO_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME).protected \
+            -targeting_RW_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME).unprotected \
+            -targeting_RW_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME).unprotected \
+            -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) \
+            -sbe_binary_dir $(SBE_BINARY_DIR) \
+            -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) \
+            -wink_binary_filename $(BR2_HOSTBOOT_BINARY_WINK_FILENAME) \
+            -occ_binary_filename $(OCC_STAGING_DIR)/$(OCC_BIN_FILENAME) \
+            -capp_binary_filename $(BINARIES_DIR)/$(BR2_CAPP_UCODE_BIN_FILENAME) \
+            -ima_catalog_binary_filename $(BINARIES_DIR)/$(BR2_IMA_CATALOG_FILENAME) \
+            -openpower_version_filename $(OPENPOWER_PNOR_VERSION_FILE) \
+            -wof_binary_filename $(OPENPOWER_MRW_SCRATCH_DIR)/$(BR2_WOFDATA_FILENAME) \
+            -memd_binary_filename $(OPENPOWER_MRW_SCRATCH_DIR)/$(BR2_MEMDDATA_FILENAME) \
+            -payload $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_NAME) \
+            -payload_filename $(BR2_SKIBOOT_LID_XZ_NAME) \
+            -binary_dir $(BINARIES_DIR) \
+            -bootkernel_filename $(LINUX_IMAGE_NAME) \
+            -pnor_layout $(@D)/"$(OPENPOWER_RELEASE)"Layouts/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) \
+            $(XZ_ARG) $(KEY_TRANSITION_ARG) $(SIGN_MODE_ARG) ; \
+	fi
+	if [ "$(BR2_OPENPOWER_PNOR_OLD_METHOD)" == "y" ]; then \
+        $(TARGET_MAKE_ENV) $(@D)/create_pnor_image.pl \
+            -release $(OPENPOWER_RELEASE) \
+            -xml_layout_file $(@D)/"$(OPENPOWER_RELEASE)"Layouts/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) \
+            -pnor_filename $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) \
+            -hb_image_dir $(HOSTBOOT_IMAGE_DIR) \
+            -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
+            -outdir $(STAGING_DIR)/pnor/ \
+            -payload $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_SKIBOOT_LID_XZ_NAME) \
+            -bootkernel $(OPENPOWER_PNOR_SCRATCH_DIR)/$(LINUX_IMAGE_NAME) \
+            -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) \
+            -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) \
+            -wink_binary_filename $(BR2_HOSTBOOT_BINARY_WINK_FILENAME) \
+            -occ_binary_filename $(OCC_STAGING_DIR)/$(OCC_BIN_FILENAME) \
+            -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME) \
+            -targeting_RO_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME).protected \
+            -targeting_RW_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME).unprotected \
+            -wofdata_binary_filename $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_WOFDATA_BINARY_FILENAME) \
+            -memddata_binary_filename $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_MEMDDATA_BINARY_FILENAME) \
+            -openpower_version_filename $(OPENPOWER_PNOR_SCRATCH_DIR)/openpower_pnor_version.bin ;\
+	fi
+
+        if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/cappucode.bin.ecc $(STAGING_DIR)/pnor/cappucode.bin.ecc ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/ima_catalog.bin.ecc $(STAGING_DIR)/pnor/ima_catalog.bin.ecc ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_SKIBOOT_LID_XZ_NAME) $(STAGING_DIR)/pnor/skiboot.lid.xz.stb ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(LINUX_IMAGE_NAME) $(STAGING_DIR)/pnor/zImage.epapr.stb ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/openpower_pnor_version.bin $(STAGING_DIR)/pnor/openpower_pnor_version.bin ; \
+	fi
+	# We don't (yet) do the STB header
+	if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot.header.bin.ecc $(STAGING_DIR)/pnor/ ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot_extended.header.bin.ecc $(STAGING_DIR)/pnor/ ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hostboot_runtime.header.bin.ecc $(STAGING_DIR)/pnor/ ; \
+	fi
+
+	# POWER9 only (or, rather, not p8)
+        if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/hbbl.bin.ecc $(STAGING_DIR)/pnor/hbbl.bin.ecc; \
+		$(TARGET_MAKE_ENV) python $(SBE_BINARY_DIR)/sbeOpDistribute.py --install \
+			--buildSbePart $(HOSTBOOT_IMAGE_DIR)/buildSbePart.pl \
+			--hw_ref_image $(HCODE_STAGING_DIR)/p9n.ref_image.bin \
+			--sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) \
+			--scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
+			--sbe_binary_dir $(SBE_BINARY_DIR) ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/ringOvd.bin $(STAGING_DIR)/pnor/ ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/SBKT.bin $(STAGING_DIR)/pnor/SBKT.bin.ecc ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_WOFDATA_BINARY_FILENAME) $(STAGING_DIR)/pnor/wofdata.bin.ecc ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_MEMDDATA_BINARY_FILENAME) $(STAGING_DIR)/pnor/memd_extra_data.bin.ecc ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_HOSTBOOT_BINARY_WINK_FILENAME) $(STAGING_DIR)/pnor/hcode.bin.ecc ; \
+		cp $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME).ecc $(STAGING_DIR)/pnor/hostboot_targeting.bin.ecc ; \
+	fi
+
+	# On P9 we don't need to pad OCC, but we do have the secboot header
+	if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
+		cp $(OCC_STAGING_DIR)/$(OCC_BIN_FILENAME).ecc $(STAGING_DIR)/pnor/occ.bin.header.ecc ; \
+	fi
+
+	if [ "$(BR2_PACKAGE_HOSTBOOT)" == "y" ]; then \
+		(cd $(STAGING_DIR)/pnor; $(TARGET_MAKE_ENV) ffspart -e -s $(BR2_OPENPOWER_PNOR_BLOCK_SIZE) -c $(BR2_OPENPOWER_PNOR_BLOCK_COUNT) -i $(abspath $(BR2_OPENPOWER_PNOR_CSV_LAYOUT_FILENAME)) -p $(STAGING_DIR)/pnor/ffspart2.pnor) ; \
+	fi
+
+	if [ "$(BR2_OPENPOWER_PNOR_OLD_METHOD)" == "y" ]; then \
+	        $(INSTALL) $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(BINARIES_DIR) ; \
+	else \
+		cp $(STAGING_DIR)/pnor/ffspart2.pnor $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) ; \
+		$(INSTALL) $(STAGING_DIR)/pnor/ffspart2.pnor $(BINARIES_DIR)/$(BR2_OPENPOWER_PNOR_FILENAME) ; \
+	fi
+
+	if [ "$(BR2_OPENPOWER_PNOR_OLD_METHOD)" == "y" ]; then \
+		if [ "$(BR2_PACKAGE_HOSTBOOT_P8)" == "y" ]; then \
+			$(TARGET_MAKE_ENV) ../openpower/scripts/pnordiff.sh $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(STAGING_DIR)/pnor/ffspart.pnor ; \
+		fi ; \
+		$(TARGET_MAKE_ENV) ../openpower/scripts/pnordiff.sh $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(STAGING_DIR)/pnor/ffspart2.pnor ; \
+	fi
 
 
         # if this config has an UPDATE_FILENAME defined, create a 32M (1/2 size)
